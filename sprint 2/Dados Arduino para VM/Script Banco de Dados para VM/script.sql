34901571efcf826c37fbdb998b384a7b970cf02f
calace <felipe.caetano@sptech.school>
2024-10-23 11:55:47 -0300

    adicionando campos adicionais no script BD

--- sprint 2/Dados Arduino para VM/Script Banco de Dados para VM/script.sql
+++ sprint 2/Dados Arduino para VM/Script Banco de Dados para VM/script.sql
@@ -3,25 +3,43 @@ create database leakgas;
 use leakgas;
 
 create table cadastro(
-    idEmpresa int primary key auto_increment,
+    idRepresentante int primary key auto_increment,
+    nome varchar(50),
+    sobrenome varchar(50),
+    telefone char(12),
+    emailLogin varchar(50),
+    senha varchar(20)
+);
+create table empresaMatriz(
+    idEmpresa int auto_increment,
     nomeFantasia varchar(50) not null,
     nomeSocial varchar(50) not null,
     CNPJ char(14) not null,
-    representanteEmpresa varchar(50),
-    emailContato varchar(50),
+    emailCorporativo varchar(50),
+    telefoneCorporativo char(12),
     unidadesEmpresa int,
-    username varchar (20),
-    password varchar (20)
+    enderecoCompleto varchar(255),
+    cep char(8),
+    complemento varchar(45),
+    cidade varchar(45),
+    estado varchar(45),
+    pais varchar(25),
+    fkRepresentante int,
+    constraint fkCadastroEmpresa foreign key(fkRepresentante) references cadastro(idRepresentante),
+    primary key(idEmpresa, fkRepresentante)
 );
+
 create table sensores(
-    idSensor int primary key auto_increment,
+    idSensor int auto_increment,
     estadoAtual varchar(10) not null,
     localizacaoSensor varchar(50),
     UnidadeEmpresa varchar(50),
     fkEmpresa int,
     constraint chkEstado check(estadoAtual in('Ativo','Inativo','Manutenção')),
-    constraint fkSensorEmpresa foreign key(fkEmpresa) references cadastro(idEmpresa)
+    constraint fkSensorEmpresa foreign key(fkEmpresa) references empresaMatriz(idEmpresa),
+    primary key(idSensor, fkEmpresa)
 );
+
 create table dadosSensores(
     idDado int primary key auto_increment,
     fkSensores int,
@@ -29,28 +47,47 @@ create table dadosSensores(
     dataHora datetime default current_timestamp,
         constraint fkDadoSensor foreign key(fkSensores) references sensores(idSensor)
 );
+
 create table propaganda(
 idEmail int primary key auto_increment,
 emailPropaganda varchar(50)
 );
 
 insert into cadastro values(
-default,
- 'McDonalds',
- 'McDonalds Brasil Ltda',
- '12345678000195',
- 'João Silva',
- 'contato@mcdonalds.com.br',
- 100,
- 'mcdonalds_user',
- 'AmoMuitoTudoIsso'
+    default,
+    'João',
+    'Silva',
+    '123456780001',
+    'contato@mcdonalds.com.br',
+    'AmoMuitoTudoIsso'
 );
 
-insert into sensores values
-(
-default,
-'Ativo',
-'Cozinha',
-'Avenida Paulista',
-1
+insert into empresaMatriz values(
+    default,
+    'Arcos Dorados',
+    'McDonalds Brasil Ltda',
+    '12345678000195',
+    'contato@mcdonalds.com.br',
+    '123456780001',
+    100,
+    'Avenida Paulista, 100',
+    '01310100',
+    'Sala 100',
+    'São Paulo',
+    'São Paulo',
+    'Brasil',
+    1  
+);
+
+insert into sensores values(
+    default,
+    'Ativo',
+    'Cozinha',
+    'Avenida Paulista',
+    1  
+);
+
+insert into propaganda values(
+    default,
+    'contato@mcdonalds.com.br'
 );
