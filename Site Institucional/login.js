c7d007f1c19adfdee85ae826bf280ec25f510474
Mateus <mateus.pereira@sptech.school>
2024-10-28 00:35:02 -0300

    realizando validação da página de login para a dashboard

--- Site Institucional/login.js
+++ Site Institucional/login.js
@@ -20,3 +20,23 @@ function goToNextPage() {
         window.location.href = "cadastro.html";
     }, 1000);
 }
+
+// Função para validação do login
+function validateLogin() {
+    // Obtém os valores dos campos de e-mail e senha
+    const email = document.querySelector('.sign-in-container input[type="email"]').value;
+    const password = document.querySelector('.sign-in-container input[type="password"]').value;
+
+    // Verifica se o e-mail e senha correspondem aos valores esperados
+    if (email === 'admin@admin' && password === 'urubu100') {
+        // Redireciona para a página desejada se os dados estiverem corretos
+        window.location.href = 'dashboard.html'; // Página linkada
+    } else {
+        // Exibe uma mensagem de erro se o login falhar
+        alert('Login ou senha inválida.');
+    }
+}
+
+// Adiciona um evento de clique ao botão de login para chamar a função de validação
+const loginButton = document.querySelector('.sign-in-container button');
+loginButton.addEventListener('click', validateLogin);
