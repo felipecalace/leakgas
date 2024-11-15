197ada09b4743a9bb89523b23c32786825d695b4
Paloma <paloma.dias@sptech.school>
2024-11-15 13:53:31 -0300

    Adiciona animação de abrir e fechar respostas de FAQ automaticamente

--- Nova_Organização/public/js/script.js
+++ Nova_Organização/public/js/script.js
@@ -8,13 +8,26 @@ questions.forEach(question => {
       answer.style.display = 'none';
     });
 
+    // Alterna as classes + e - no ícone
+    questions.forEach(q => {
+      const icon = q.querySelector('i');
+      const currentAnswer = q.nextElementSibling;
+      
+      // Se a pergunta não for a clicada, o ícone volta ao estado inicial e a resposta é escondida
+      if (q !== question) {
+        icon.classList.remove('minus');
+        icon.classList.add('plus');
+        currentAnswer.style.display = 'none';
+      }
+    });
+
     // Faz a resposta da pergunta clicada aparecer
     const answer = question.nextElementSibling;
     if (answer.style.display !== 'block') {
       answer.style.display = 'block';
     }
 
-    // Alterna as classes + e - no ícone
+    // Alterna as classes + e - no ícone da pergunta clicada
     const icon = question.querySelector('i');
     if (icon.classList.contains('plus')) {
       icon.classList.remove('plus');
@@ -22,7 +35,7 @@ questions.forEach(question => {
     } else {
       icon.classList.remove('minus');
       icon.classList.add('plus');
-      answer.style.display = 'none'; // Fecha a resposta se o ícone for clicado novamente
+      answer.style.display = 'none';
     }
   });
 });
