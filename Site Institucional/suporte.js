e42c393e4042023fa48be1c863d1048d8191fd0b
mateus roque <mateus.pereira@sptech.school>
2024-11-16 13:47:26 -0300

    ajustando página suporte, tirando navbar e implementando botão de sair

--- Site Institucional/suporte.js
+++ Site Institucional/suporte.js
@@ -1,14 +1,8 @@
 function toggleSidebar() {
-    const sidebar = document.getElementById("sidebar");
-    const content = document.querySelector(".content");
-  
-    // Alterna entre as classes "expanded" para expandir ou recolher a sidebar
-    sidebar.classList.toggle("expanded");
-    content.classList.toggle("expanded");
-  }
-  
-  function toggleButton() {
-    const button = document.querySelector('.floating-button');
-    button.classList.toggle('expanded');
-  }
-  
+  const sidebar = document.getElementById("sidebar");
+  const content = document.querySelector(".container");
+
+  // Alterna a classe "expanded"
+  sidebar.classList.toggle("expanded");
+  content.classList.toggle("expanded");
+}
