b577865a2970ac19dbef49ecbd92bf578284bbd9
mateus roque <mateus.pereira@sptech.school>
2024-11-19 13:05:21 -0300

    adicionando formulário para inserção do bob-ia na página suporte

--- Site Institucional/suporte.js
+++ Site Institucional/suporte.js
@@ -6,3 +6,23 @@ function toggleSidebar() {
   sidebar.classList.toggle("expanded");
   content.classList.toggle("expanded");
 }
+
+// Abrir o pop-up
+function toggleButton() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'flex';
+}
+
+// Fechar o pop-up
+function closePopup() {
+  const popup = document.getElementById('popup');
+  popup.style.display = 'none';
+}
+
+// Formulário de envio (opcional)
+const supportForm = document.getElementById('supportForm');
+supportForm.addEventListener('submit', function (e) {
+  e.preventDefault();
+  alert('Formulário enviado com sucesso!');
+  closePopup();
+});
